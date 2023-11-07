17daad3016d8203e49923395d02bb141e94fab5a
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-11-08 00:22:57 +0700

    feat(superadmin): create routes

---
+++ routes/superAdminRoutes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"os"
+	"qbills/handler"
+	"qbills/repository"
+	"qbills/services"
+	"qbills/utils/helpers/middleware"
+
+	"github.com/go-playground/validator"
+	echoJwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func SuperAdminRoutes(e *echo.Echo, db *gorm.DB, validate *validator.Validate) {
+	superAdminRepository := repository.NewSuperAdminRepository(db)
+	superAdminService := services.NewSuperAdminService(superAdminRepository, validate)
+	SuperAdminHandler := handler.NewSuperAdminHandler(superAdminService)
+
+	superAdminGroup := e.Group("api/v1/super-admin")
+
+	superAdminGroup.POST("/login", SuperAdminHandler.LoginSuperAdminHandler)
+
+	superAdminGroup.Use(echoJwt.JWT([]byte(os.Getenv("SECRET_KEY"))))
+
+	superAdminGroup.GET("/:id", SuperAdminHandler.GetSuperAdminHandler, middleware.AuthMiddleware("SuperAdmin"))
+	superAdminGroup.GET("s", SuperAdminHandler.GetSuperAdminsHandler, middleware.AuthMiddleware("SuperAdmin"))
+}
