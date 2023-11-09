870f2c127021755fb3a9f794d705bf4ea6f79189
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-11-09 14:49:09 +0700

    feat(convert-point): add schema

---
+++ models/schema/convertPoint.go
@@ -0,0 +1,17 @@
+package schema
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type ConvertPoint struct {
+	ID        uint           `gorm:"primaryKey"`
+	CreatedAt time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	Point uint `json:"point"`
+	PointValue int `json:"point_value"`
+}
