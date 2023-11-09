896e63209a2dca959721df52472ec891e3040486
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-11-09 23:54:02 +0700

    feat(convertPoint): add routes

--- app.go
+++ app.go
@@ -34,6 +34,7 @@ func main() {
 
 	routes.AdminRoutes(myApp, drivers.DB, validate)
 	routes.SuperAdminRoutes(myApp, drivers.DB, validate)
+	routes.ConvertPointRoutes(myApp, drivers.DB, validate)
 
 	myApp.Pre(middleware.RemoveTrailingSlash())
 	myApp.Use(middleware.CORS())
