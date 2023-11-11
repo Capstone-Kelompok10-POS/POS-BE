6fbd85f498e073017b7c04cef3ce50e647702159
Abdul Halim Arif <53304120+ahadaulay@users.noreply.github.com>
2023-11-11 23:05:21 +0700

    feat(Product):add middleware at routes

--- routes/productRoutes.go
+++ routes/productRoutes.go
@@ -2,11 +2,14 @@ package routes
 
 import (
 	"github.com/go-playground/validator"
+	echoJwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
+	"os"
 	"qbills/handler"
 	"qbills/repository"
 	"qbills/services"
+	"qbills/utils/helpers/middleware"
 )
 
 func ProductRoutes(e *echo.Echo, db *gorm.DB, validate *validator.Validate) {
@@ -15,11 +18,12 @@ func ProductRoutes(e *echo.Echo, db *gorm.DB, validate *validator.Validate) {
 	ProductHandler := handler.NewProductHandler(productService)
 
 	Group := e.Group("api/v1/product")
+	Group.Use(echoJwt.JWT([]byte(os.Getenv("SECRET_KEY"))))
 
-	Group.POST("", ProductHandler.CreateProductHandler)
+	Group.POST("", ProductHandler.CreateProductHandler, middleware.AuthMiddleware("Admin"))
 	Group.GET("", ProductHandler.GetProductsHandler)
 	Group.GET("/:id", ProductHandler.GetProductHandler)
 	Group.GET("/search/:name", ProductHandler.GetProductByNameHandler)
-	Group.PUT("/:id", ProductHandler.UpdateProductHandler)
-	Group.DELETE("/:id", ProductHandler.DeleteProductHandler)
+	Group.PUT("/:id", ProductHandler.UpdateProductHandler, middleware.AuthMiddleware("Admin"))
+	Group.DELETE("/:id", ProductHandler.DeleteProductHandler, middleware.AuthMiddleware("Admin"))
 }
