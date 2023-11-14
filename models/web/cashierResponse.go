44b829e2bb96eee9c7115176de399f16b7194193
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-11-14 18:20:58 +0700

    feat(cashier): update web response

--- models/web/cashierResponse.go
+++ models/web/cashierResponse.go
@@ -1,13 +1,14 @@
 package web
 
 type CashierLoginResponse struct {
+	Fullname string `json:"fullname"`
 	Username string `json:"username"`
-	Token string `json:"token"`
+	Token    string `json:"token"`
 }
 
 type CashierResponse struct {
-	ID uint `json:"id"`
-	AdminID uint `json:"adminID"`
+	ID       uint   `json:"id"`
+	AdminID  uint   `json:"adminID"`
 	Fullname string `json:"fullname"`
 	Username string `json:"username"`
 }
