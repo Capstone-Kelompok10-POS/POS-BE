255493c02f830eec3d9953a7b980bac6365de160
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-11-17 18:53:06 +0700

    feat(development) : change variabel and function name

--- models/web/membershipResponse.go
+++ models/web/membershipResponse.go
@@ -2,9 +2,25 @@ package web
 
 // Membership Response
 type MembershipResponse struct {
+<<<<<<< Updated upstream
 	ID           uint   `json:"id"`
 	CashierID    uint   `json:"CashierID"`
 	Name         string `json:"name"`
 	Point        uint   `json:"point"`
 	Phone_Number string `json:"phone_number"`
+=======
+<<<<<<< Updated upstream
+	ID        uint   `json:"id"`
+	CashierID uint   `json:"CashierID"`
+	Name      string `json:"name"`
+	Point     uint   `json:"point"`
+	Telephone string `json:"telephone"`
+=======
+	ID           uint   `json:"id"`
+	CashierID    uint   `json:"cashierID"`
+	Name         string `json:"name"`
+	Point        uint   `json:"point"`
+	Phone_Number string `json:"phone_number"`
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
