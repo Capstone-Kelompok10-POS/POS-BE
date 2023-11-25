433d82603ccab065ad81e64cfea7c26af0111352
Abdul Halim Arif <53304120+ahadaulay@users.noreply.github.com>
2023-11-25 21:45:45 +0700

    feat(product):fixing membership model

--- models/web/membershipRequest.go
+++ models/web/membershipRequest.go
@@ -1,42 +1,14 @@
 package web
 
-<<<<<<< Updated upstream
-//Membership Request
-=======
-<<<<<<< Updated upstream
-//Membership Request 
->>>>>>> Stashed changes
 type MembershipCreateRequest struct {
-	CashierID    uint   `json:"CashierID"`
-	Name         string `json:"name" validate:"required,min=1,max=255"`
-	Phone_Number string `json:"phone_number" validate:"required,min=1,max=15"`
+	CashierID   uint   `json:"CashierID"`
+	Name        string `json:"name" validate:"required,min=1,max=255"`
+	PhoneNumber string `json:"phoneNumber" validate:"required,min=1,max=15"`
 }
 
 type MembershipUpdateRequest struct {
-<<<<<<< Updated upstream
-	CashierID    uint   `json:"CashierID"`
-=======
-	CashierID	uint	`json:"CashierID"`
-	Name		string 	`json:"name" validate:"required,min=1,max=255"`
-	Point       uint    `json:"point"`
-	Telephone	string	`json:"telephone" validate:"required,min=1,max=15"`
+	CashierID   uint   `json:"CashierID"`
+	Name        string `json:"name" validate:"required,min=1,max=255"`
+	Point       uint   `json:"point"`
+	PhoneNumber string `json:"phoneNumber" validate:"required,min=1,max=15"`
 }
-=======
-//Membership Request
-type MembershipCreateRequest struct {
-	CashierID    uint   `json:"cashierID"`
-	Name         string `json:"name" validate:"required,min=1,max=255"`
-	Phone_Number string `json:"phone_number" validate:"required,min=1,max=15"`
-}
-
-type MembershipUpdateRequest struct {
-	CashierID    uint   `json:"cashierID"`
->>>>>>> Stashed changes
-	Name         string `json:"name" validate:"required,min=1,max=255"`
-	Point        uint   `json:"point"`
-	Phone_Number string `json:"phone_number" validate:"required,min=1,max=15"`
-}
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
--- models/web/membershipResponse.go
+++ models/web/membershipResponse.go
@@ -2,25 +2,9 @@ package web
 
 // Membership Response
 type MembershipResponse struct {
-<<<<<<< Updated upstream
-	ID           uint   `json:"id"`
-	CashierID    uint   `json:"CashierID"`
-	Name         string `json:"name"`
-	Point        uint   `json:"point"`
-	Phone_Number string `json:"phone_number"`
-=======
-<<<<<<< Updated upstream
-	ID        uint   `json:"id"`
-	CashierID uint   `json:"CashierID"`
-	Name      string `json:"name"`
-	Point     uint   `json:"point"`
-	Telephone string `json:"telephone"`
-=======
-	ID           uint   `json:"id"`
-	CashierID    uint   `json:"cashierID"`
-	Name         string `json:"name"`
-	Point        uint   `json:"point"`
-	Phone_Number string `json:"phone_number"`
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+	ID          uint   `json:"id"`
+	CashierID   uint   `json:"CashierID"`
+	Name        string `json:"name"`
+	Point       uint   `json:"point"`
+	PhoneNumber string `json:"phoneNumber"`
 }
