a923a87393b760e7c16f440f75bb8229cb9a945e
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-02 17:54:33 +0700

    fix(productDetail): fixxing request and response web
    add logrus, fix request validation, fix response web

--- models/web/productDetailRequest.go
+++ models/web/productDetailRequest.go
@@ -1,8 +1,14 @@
 package web
 
 type ProductDetailCreate struct {
+<<<<<<< Updated upstream
 	ProductID  uint    `json:"productID"`
 	Price      float64 `json:"price" `
 	TotalStock int     `json:"totalStock"`
+=======
+	ProductID  uint    `json:"productId"`
+	Price      float64 `json:"price"`
+	TotalStock int     `json:"totalStock" validate:"numeric"`
+>>>>>>> Stashed changes
 	Size       string  `json:"size"`
 }
--- repository/productDetailRepository.go
+++ repository/productDetailRepository.go
@@ -1,19 +1,35 @@
 package repository
 
 import (
+<<<<<<< Updated upstream
 	"gorm.io/gorm"
+=======
+>>>>>>> Stashed changes
 	"qbills/models/domain"
 	"qbills/models/schema"
 	req "qbills/utils/request"
 	res "qbills/utils/response"
+<<<<<<< Updated upstream
+=======
+
+	"gorm.io/gorm"
+>>>>>>> Stashed changes
 )
 
 type ProductDetailRepository interface {
 	Create(productDetail *domain.ProductDetail) (*domain.ProductDetail, error)
 	Update(productDetail *domain.ProductDetail, id uint) (*domain.ProductDetail, error)
+<<<<<<< Updated upstream
 	FindById(id uint) (*domain.ProductDetail, error)
 	FindAll() ([]domain.ProductDetail, error)
 	Delete(id uint) error
+=======
+	StockDecrease(tx *gorm.DB, productDetail *domain.ProductDetail) error
+	FindById(id uint) (*domain.ProductDetail, error)
+	FindAll() ([]domain.ProductDetail, error)
+	Delete(id uint) error
+	FindAllByIds(ids []uint) ([]domain.ProductDetail, error)
+>>>>>>> Stashed changes
 }
 
 type ProductDetailRepositoryImpl struct {
@@ -48,15 +64,35 @@ func (repository *ProductDetailRepositoryImpl) Update(productDetail *domain.Prod
 	return productDetail, nil
 }
 
+<<<<<<< Updated upstream
 func (repository *ProductDetailRepositoryImpl) FindById(id uint) (*domain.ProductDetail, error) {
 	product := domain.ProductDetail{}
 
 	result := repository.DB.Where("deleted_at IS NULL").First(&product, id)
+=======
+func (repository *ProductDetailRepositoryImpl) StockDecrease(tx *gorm.DB, productDetail *domain.ProductDetail) error {
+    result := tx.Table("product_details").Where("id = ?", productDetail.ID).Where("deleted_at IS NULL").Update("total_stock", productDetail.TotalStock)
+    if result.Error != nil {
+        return result.Error
+    }
+
+    return nil
+}
+
+func (repository *ProductDetailRepositoryImpl) FindById(id uint) (*domain.ProductDetail, error) {
+	productDetail := domain.ProductDetail{}
+
+	result := repository.DB.Where("deleted_at IS NULL").First(&productDetail, id)
+>>>>>>> Stashed changes
 
 	if result.Error != nil {
 		return nil, result.Error
 	}
+<<<<<<< Updated upstream
 	return &product, nil
+=======
+	return &productDetail, nil
+>>>>>>> Stashed changes
 }
 
 func (repository *ProductDetailRepositoryImpl) FindAll() ([]domain.ProductDetail, error) {
@@ -79,3 +115,17 @@ func (repository *ProductDetailRepositoryImpl) Delete(id uint) error {
 	}
 	return nil
 }
+<<<<<<< Updated upstream
+=======
+
+func (repository *ProductDetailRepositoryImpl) FindAllByIds(ids []uint) ([]domain.ProductDetail, error) {
+	var products []domain.ProductDetail
+
+	result := repository.DB.Preload("Product").Where("deleted_at IS NULL").Find(&products, ids)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return products, nil
+}
+>>>>>>> Stashed changes
