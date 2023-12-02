e57be99816b27e4f313664f0d99a99e93290070f
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-02 17:49:54 +0700

    fix(paymentMethod): fixxing request and response web
    add logrus, fix request validation, fix response web

--- models/web/paymentMethodResponse.go
+++ models/web/paymentMethodResponse.go
@@ -2,6 +2,16 @@ package web
 
 type PaymentMethodResponse struct {
 	ID            uint   `json:"id"`
+<<<<<<< Updated upstream
 	PaymentTypeID uint   `gorm:"not null" json:"paymentTypeID"`
 	Name          string `gorm:"not null" json:"name"`
+=======
+<<<<<<< Updated upstream
+	PaymentTypeID uint   `gorm:"not null" json:"paymentTypeID"`
+	Name          string `gorm:"not null" json:"name"`
+=======
+	PaymentTypeID uint   `json:"paymentTypeId"`
+	Name          string `json:"name"`
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
