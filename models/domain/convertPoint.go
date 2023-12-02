cf44b6b81b8fbe697941fe62f41a933f6affc4fd
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-02 17:48:19 +0700

    fix(convertpoint): fixxing domain, handler
    change type variabel and logrus

--- models/domain/convertPoint.go
+++ models/domain/convertPoint.go
@@ -2,6 +2,14 @@ package domain
 
 type ConvertPoint struct {
 	ID         uint
+<<<<<<< Updated upstream
 	Point      uint
+=======
+<<<<<<< Updated upstream
+	Point      uint
+=======
+	Point      int
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 	ValuePoint int
 }
