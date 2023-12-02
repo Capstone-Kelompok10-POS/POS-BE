924b5e7ff4df5c5b7c0a4f1c6ca1135c42944e4e
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-02 17:55:16 +0700

    fix(convertPoint): fixxing request and response web
    add logrus, fix request validation, fix response web

--- models/web/convertPointResponse.go
+++ models/web/convertPointResponse.go
@@ -2,6 +2,21 @@ package web
 
 type ConvertPointResponse struct {
 	ID         uint `json:"id"`
+<<<<<<< Updated upstream
 	Point      uint `json:"point"`
 	ValuePoint int  `json:"value_point"`
+=======
+<<<<<<< Updated upstream
+	Point      uint `json:"point"`
+	ValuePoint int  `json:"value_point"`
+=======
+<<<<<<< Updated upstream
+	Point      uint `json:"point"`
+	ValuePoint int  `json:"value_point"`
+=======
+	Point      int  `json:"point"`
+	ValuePoint int  `json:"valuePoint"`
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
