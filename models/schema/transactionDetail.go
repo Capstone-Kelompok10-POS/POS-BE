feaece81e5f3b52f0c355c47fa657340b8369de7
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-02 18:00:44 +0700

    feat(transactionDetail): add schema

---
+++ models/schema/transactionDetail.go
@@ -0,0 +1,16 @@
+package schema
+
+import "time"
+
+type TransactionDetail struct {
+	ID              uint      	  `gorm:"primaryKey"`
+	CreatedAt       time.Time 	  `gorm:"autoCreateTime"`
+
+	TransactionID   uint          `gorm:"index;not null"`
+	ProductDetailID uint          `gorm:"index;not null"`
+	ProductDetail   ProductDetail `gorm:"foreignKey:ProductDetailID"`
+	Price           float64       `json:"price" gorm:"type:decimal(10,2);not null"`
+	Quantity        int           `json:"quantity" gorm:"not null"`
+	SubTotal        float64       `json:"subtotal" gorm:"type:decimal(10,2);not null"`
+	Notes           string        `json:"notes"`
+}
