06d183170e6144d421bee2fc4fe98173ef3eb31e
Mohammad Arief Lazuardi <113014584+AriefLazuardi@users.noreply.github.com>
2023-12-03 16:45:20 +0700

    fix conflict productDetailResponse.go

--- models/web/productDetailResponse.go
+++ models/web/productDetailResponse.go
@@ -1,39 +1,25 @@
 package web
 
 type ProductDetailResponse struct {
-<<<<<<< Updated upstream
-	ID         uint
-	ProductID  uint
-=======
 	ID         uint    `json:"id"`
 	ProductID  uint    `json:"productId"`
->>>>>>> Stashed changes
 	Price      float64 `json:"price" `
 	TotalStock int     `json:"totalStock"`
 	Size       string  `json:"size"`
 }
 
 type ProductDetailCreateResponse struct {
-<<<<<<< Updated upstream
-	ID         uint
-	ProductID  uint
-=======
 	ID         uint    `json:"id"`
 	ProductID  uint    `json:"productId"`
->>>>>>> Stashed changes
 	Price      float64 `json:"price" `
 	TotalStock int     `json:"totalStock"`
 	Size       string  `json:"size"`
 }
-<<<<<<< Updated upstream
-=======
-
 type ProductDetailTransactionResponse struct {
-	ID         uint 					  `json:"id"`
-	ProductID  uint 					  `json:"productId"`
+	ID         uint `json:"id"`
+	ProductID  uint `json:"productId"`
 	Product    ProductTransactionResponse `json:"product"`
-	Price      float64 					  `json:"price" `
-	TotalStock int     					  `json:"totalStock"`
-	Size       string  					  `json:"size"`
+	Price      float64 `json:"price"`
+	TotalStock int     `json:"totalStock"`
+	Size       string  `json:"size"`
 }
->>>>>>> Stashed changes
