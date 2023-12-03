a700314b5a44a7e1a732995fa2aa6dd7907760e8
Mohammad Arief Lazuardi <113014584+AriefLazuardi@users.noreply.github.com>
2023-12-03 16:46:43 +0700

    fix code conflict productDetail.go

--- models/domain/productDetail.go
+++ models/domain/productDetail.go
@@ -3,10 +3,7 @@ package domain
 type ProductDetail struct {
 	ID         uint
 	ProductID  uint
-<<<<<<< Updated upstream
-=======
 	Product    Product
->>>>>>> Stashed changes
 	Price      float64
 	TotalStock int
 	Size       string
