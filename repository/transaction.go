6e51bfec450e862de2bd29da3e13da3c3b402a68
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 15:59:29 +0700

    feat(transaction): update repository
    add payment manual , add notification real time

--- repository/transaction.go
+++ repository/transaction.go
@@ -2,7 +2,10 @@ package repository
 
 import (
 	"fmt"
+	"os"
 	"qbills/models/domain"
+	"qbills/utils/helpers"
+	"strconv"
 
 	"gorm.io/gorm"
 )
@@ -11,7 +14,10 @@ type TransactionRepository interface {
 	BeginTransaction() *gorm.DB
 	Save(transaction *domain.Transaction) (*domain.Transaction, error)
 	FindById(transactionID int) (*domain.Transaction, error)
-	// UpdateTransactionStatus(transaction domain.) (*domain.Transaction, error)
+	UpdateStatusTransactionPayment(status string, transactionResult *domain.PaymentTransactionStatus) error
+	FindAllTransaction() ([]domain.Transaction, int, error)
+	FindByInvoice(invoice string) (*domain.Transaction, error)
+	FindPaginationTransaction(orderBy string, paginate helpers.Pagination) ([]domain.Transaction, *helpers.Pagination, error)
 }
 
 type TransactionRepositoryImpl struct {
@@ -53,7 +59,7 @@ func (repository *TransactionRepositoryImpl) Save(transaction *domain.Transactio
 func (repository *TransactionRepositoryImpl) FindById(transactionID int) (*domain.Transaction, error) {
 	transaction := domain.Transaction{}
 
-	result := repository.DB.Preload("Cashier").Preload("Membership").Preload("ConvertPoint").Preload("Details").First(&transaction, transactionID)
+	result := repository.DB.Preload("Cashier").Preload("Membership").Preload("ConvertPoint").Preload("Details").Preload("TransactionPayment.PaymentMethod").Preload("TransactionPayment.PaymentMethod.PaymentType").First(&transaction, transactionID)
 	if result.Error != nil {
 		return nil, result.Error
 	}
@@ -68,5 +74,81 @@ func (repository *TransactionRepositoryImpl) FindById(transactionID int) (*domai
 	return &transaction, nil
 }
 
+func (repository *TransactionRepositoryImpl) UpdateStatusTransactionPayment(status string, transactionResult *domain.PaymentTransactionStatus) error {
+	result := repository.DB.Table("transaction_payments").Where("invoice = ?", transactionResult.OrderID).Updates(&domain.TransactionPayment{
+		PaymentStatus: status,
+		UpdatedAt: transactionResult.SettlementTime,
+	})
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (repository *TransactionRepositoryImpl) FindByInvoice(invoice string) (*domain.Transaction, error) {
+	transaction := domain.Transaction{}
+
+	result := repository.DB.
+		Preload("Cashier").
+		Preload("Membership").
+		Preload("ConvertPoint").
+		Preload("Details.ProductDetail.Product").
+		Preload("Details.ProductDetail").
+		Preload("TransactionPayment.PaymentMethod").
+		Preload("TransactionPayment.PaymentMethod.PaymentType").
+		Joins("LEFT JOIN transaction_payments ON transactions.id = transaction_payments.transaction_id").
+		Where("transactions.deleted_at IS NULL AND transaction_payments.invoice = ?", invoice).Find(&transaction)
+	if result.Error != nil {
+		return nil, result.Error
+	}
 
 
+	return &transaction, nil
+}
+
+func (repository *TransactionRepositoryImpl) FindAllTransaction() ([]domain.Transaction, int, error) {
+	transactions := []domain.Transaction{}
+
+	result := repository.DB.Preload("Cashier").Preload("Membership").Preload("ConvertPoint").Preload("Details.ProductDetail.Product").Preload("Details.ProductDetail").Preload("TransactionPayment.PaymentMethod").Preload("TransactionPayment.PaymentMethod.PaymentType").Where("deleted_at IS NULL").Find(&transactions)
+	if result.Error != nil {
+		return nil, 0,  result.Error
+	}
+
+	totalTransaction := len(transactions)
+
+	return transactions, totalTransaction , nil
+}
+
+func (repository *TransactionRepositoryImpl) FindPaginationTransaction(orderBy string, paginate helpers.Pagination) ([]domain.Transaction, *helpers.Pagination, error) {
+	var transactions []domain.Transaction
+
+	result := repository.DB.Scopes(helpers.Paginate(transactions, &paginate, repository.DB)).Preload("Cashier").Preload("Membership").Preload("ConvertPoint").Preload("Details.ProductDetail.Product").Preload("Details.ProductDetail").Preload("TransactionPayment.PaymentMethod").Preload("TransactionPayment.PaymentMethod.PaymentType").Where("deleted_at IS NULL")
+
+	if orderBy != "" {
+		result.Order("name" + orderBy).Find(&transactions)
+	} else {
+		result.Find(&transactions)
+	}
+
+	if result.Error != nil {
+		return nil, nil, result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return nil, nil, fmt.Errorf("transactions is empty")
+	}
+
+	if paginate.Page <= 1 {
+		paginate.PreviousPage = ""
+	} else {
+		paginate.PreviousPage = os.Getenv("MAIN_URL") + "/api/" + os.Getenv("API_VERSION") + "/transactions/pagination?page=" + strconv.Itoa(int(paginate.Page)-1)
+	}
+
+	if paginate.Page >= paginate.TotalPage {
+		paginate.NextPage = ""
+	} else {
+		paginate.NextPage = os.Getenv("MAIN_URL") + "/api/" + os.Getenv("API_VERSION") + "/transactions/pagination?page=" + strconv.Itoa(int(paginate.Page)+1)
+	}
+
+	return transactions, &paginate, nil
+}
