e36c0cd0788fecbb33b2dd3b445e12b9ebde4a2e
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 16:08:33 +0700

    feat(product): update web response

--- models/web/productResponse.go
+++ models/web/productResponse.go
@@ -10,7 +10,7 @@ type ProductResponse struct {
 	Admin         domain.Admin       `json:"admin"`
 	ProductDetail []domain.ProductDetail
 	Name          string `json:"name"`
-	Ingredients   string `json:"ingredients" gorm:"not null"`
+	Ingredients   string `json:"ingredients"`
 	Image         string `json:"image"`
 }
 
@@ -22,23 +22,48 @@ type ProductResponseCustom struct {
 	AdminID       uint                 `json:"adminId"`
 	Admin         domain.AdminResponse `json:"admin"`
 	Name          string               `json:"name"`
-	Ingredients   string               `json:"ingredients" gorm:"not null"`
+	Ingredients   string               `json:"ingredients"`
 	Image         string               `json:"image"`
 }
 
 type ProductCreateResponse struct {
 	ID            uint   `json:"Id"`
 	ProductTypeID uint   `json:"productTypeId"`
+<<<<<<< Updated upstream
 	AdminID       uint   `json:"adminID"`
+=======
+	AdminID       uint   `json:"adminId"`
+>>>>>>> Stashed changes
 	Name          string `json:"name"`
-	Ingredients   string `json:"ingredients" gorm:"not null"`
+	Ingredients   string `json:"ingredients"`
 	Image         string `json:"image"`
 }
 
 type ProductUpdateResponse struct {
+<<<<<<< Updated upstream
 	ID            uint   `json:"Id"`
 	ProductTypeID uint   `json:"productTypeId"`
+=======
+	ID            uint   `json:"id"`
+	ProductTypeID uint   `json:"productTypeid"`
+>>>>>>> Stashed changes
 	Name          string `json:"name"`
-	Ingredients   string `json:"ingredients" gorm:"not null"`
+	Ingredients   string `json:"ingredients"`
 	Image         string `json:"image"`
+}
+
+type ProductTransactionResponse struct {
+	ID            uint   `json:"id"`
+	Name          string `json:"name"`
+	Ingredients   string `json:"ingredients"`
+	Image         string `json:"image"`
+}
+
+type ProductsResponse struct {
+	ID            uint               `json:"Id"`
+	ProductType   ProductTypeResponse `json:"productType"`
+	Name          string               `json:"name"`
+	Ingredients   string               `json:"ingredients"`
+	Image         string               `json:"image"`
+	ProductDetail []ProductDetailResponse `json:"productDetail"`
 }
