fa02c0f8e24c3344370f43186f421d17f8480ebb
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 15:59:05 +0700

    feat(transaction): update web response

--- models/web/transactionResponse.go
+++ models/web/transactionResponse.go
@@ -1,19 +1,15 @@
 package web
 
-import (
-	"time"
-)
-
 type TransactionResponse struct {
-	ID             uint                          `json:"id"`
-	CreatedAt      time.Time 				     `json:"createdAt"`
-	Cashier        CashierTransactionResponse    `json:"cashier"`
-	Membership     MembershipTransactionResponse `json:"membership"`
-	ConvertPointID uint      				     `json:"convertPointId"`
-	Status         string    				     `json:"status"`
-	Discount       float64   				     `json:"discount"`
-	TotalPrice     float64   				     `json:"totalPrice"`
-	Tax     	   float64   				     `json:"tax"`
-	TotalPayment   float64 					     `json:"totalPayment"`
-	Details        []TransactionDetailResponse   `json:"details"`
+	ID                 uint                          `json:"id"`
+	CreatedAt          string                        `json:"createdAt"`
+	Cashier            CashierTransactionResponse    `json:"cashier"`
+	Membership         MembershipTransactionResponse `json:"membership"`
+	ConvertPointID     uint                          `json:"convertPointId"`
+	Discount           float64                       `json:"discount"`
+	TotalPrice         float64                       `json:"totalPrice"`
+	Tax                float64                       `json:"tax"`
+	TotalPayment       float64                       `json:"totalPayment"`
+	Details            []TransactionDetailResponse   `json:"details"`
+	TransactionPayment TransactionPaymentResponse    `json:"transactionPayment"`
 }
