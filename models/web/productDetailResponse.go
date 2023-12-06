d7bdc3dd3b6c58f49ab5d51d6f56dc912c9aa613
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 16:09:52 +0700

    feat(productDetail): update web response

--- models/web/productDetailResponse.go
+++ models/web/productDetailResponse.go
@@ -11,15 +11,33 @@ type ProductDetailResponse struct {
 type ProductDetailCreateResponse struct {
 	ID         uint    `json:"id"`
 	ProductID  uint    `json:"productId"`
+<<<<<<< Updated upstream
 	Price      float64 `json:"price" `
 	TotalStock int     `json:"totalStock"`
 	Size       string  `json:"size"`
 }
 type ProductDetailTransactionResponse struct {
+<<<<<<< Updated upstream
 	ID         uint `json:"id"`
 	ProductID  uint `json:"productId"`
 	Product    ProductTransactionResponse `json:"product"`
 	Price      float64 `json:"price"`
+=======
+	ID        uint `json:"id"`
+	ProductID uint `json:"productId"`
+	// Product    ProductTransactionResponse `json:"product"`
+=======
+>>>>>>> Stashed changes
+	Price      float64 `json:"price" `
+>>>>>>> Stashed changes
 	TotalStock int     `json:"totalStock"`
 	Size       string  `json:"size"`
+}
+type ProductDetailTransactionResponse struct {
+	ID         uint                       `json:"id"`
+	ProductID  uint                       `json:"productId"`
+	Product    ProductTransactionResponse `json:"product"`
+	Price      float64                    `json:"price"`
+	TotalStock int                        `json:"totalStock"`
+	Size       string                     `json:"size"`
 }
