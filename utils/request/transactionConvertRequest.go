2f0520d1cd4304bf7881f12720f378c8b7d535b6
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 16:01:51 +0700

    feat(transaction): update convert request

--- utils/request/transactionConvertRequest.go
+++ utils/request/transactionConvertRequest.go
@@ -1,10 +1,15 @@
 package request
 
 import (
+	"fmt"
 	"log"
 	"qbills/models/domain"
 	"qbills/models/schema"
 	"qbills/models/web"
+	"time"
+
+	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/coreapi"
 )
 
 func TransactionCreateRequestToTransactionDomain(request web.TransactionCreateRequest, create web.TransactionCreate, detail web.TransactionDetailCreate) *domain.Transaction {
@@ -12,12 +17,12 @@ func TransactionCreateRequestToTransactionDomain(request web.TransactionCreateRe
 		CashierID: request.CashierID,
 		MembershipID:       request.MembershipID,
 		ConvertPointID :    request.ConvertPointID,
-		Status:             create.Status,
 		Discount: 			create.Discount,
 		TotalPrice: 		create.TotalPrice,
 		Tax: 				create.Tax,
 		TotalPayment: 		create.TotalPayment,
 		Details:   			make([]domain.TransactionDetail, 0),
+		TransactionPayment: request.TransactionPayment,
 	}
 	for _, detailRequest:= range request.Details {
 		productDetailId := detailRequest.ProductDetailID
@@ -47,12 +52,12 @@ func TransactionDomainToTransactionSchema(request domain.Transaction) *schema.Tr
 		CashierID: request.CashierID,
 		MembershipID:       request.MembershipID,
 		ConvertPointID :    request.ConvertPointID,
-		Status : request.Status,
 		Discount: request.Discount,
 		TotalPrice: request.TotalPrice,
 		Tax: request.Tax,
 		TotalPayment: request.TotalPayment,
 	}
+
 	for _, detailRequest:= range request.Details {
 		transactionDetail := schema.TransactionDetail{
 			ProductDetailID: detailRequest.ProductDetailID,
@@ -62,4 +67,87 @@ func TransactionDomainToTransactionSchema(request domain.Transaction) *schema.Tr
 		transaction.Details = append(transaction.Details, transactionDetail)
 	}
 	return transaction
+}
+
+func CreateTransactionPaymentRequestToMidtransChargeRequest(transaction *domain.Transaction, membership *domain.Membership, paymentMethod *domain.PaymentMethod, discount, tax float64) *coreapi.ChargeReq{
+	var midtransItemDetails []midtrans.ItemDetails
+	for _, detailRequest:= range transaction.Details {
+		item := midtrans.ItemDetails{
+			ID: fmt.Sprintf("PRODUCT-%d", detailRequest.ProductDetailID),
+			Name: "PRODUCT",
+			Price: int64(detailRequest.Price),
+			Qty: int32(detailRequest.Quantity),
+			Brand: "QBILLS",
+			MerchantName: "QBILLS",
+		}
+		midtransItemDetails = append(midtransItemDetails, item)
+
+	}
+	midtransItemDetails = append(midtransItemDetails, midtrans.ItemDetails{
+		ID: "Tax",
+		Name: "Tax",
+		Price: int64(tax),
+		Qty: 1,
+		Brand: "QBILLS",
+		MerchantName: "QBILLS",
+	})
+
+	midtransItemDetails = append(midtransItemDetails, midtrans.ItemDetails{
+		ID: "Discount",
+		Name: "Discount",
+		Price: int64(-discount),
+		Qty: 1,
+		Brand: "QBILLS",
+		MerchantName: "QBILLS",
+	})
+
+	chargeRequest := coreapi.ChargeReq{
+		PaymentType: coreapi.PaymentTypeBankTransfer,
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  transaction.TransactionPayment.Invoice,
+			GrossAmt: int64(transaction.TotalPayment), // Ubah tipe data ke int64
+		},
+		CustomerDetails: &midtrans.CustomerDetails{
+			FName: membership.Name,
+			Phone: membership.PhoneNumber,
+		},
+		BankTransfer: &coreapi.BankTransferDetails{
+			Bank: midtrans.Bank(paymentMethod.Name),
+
+		},
+		Items: &midtransItemDetails, // Items bukan pointer
+	}
+	return &chargeRequest
+}
+
+func ChargeResponseToTransactionPayment(response *coreapi.ChargeResponse, transaction *domain.Transaction) *domain.Transaction {
+	parseTransactionTime, _ := time.Parse("2006-01-02 15:04:05", response.TransactionTime)
+	var vaNumber string
+	if transaction.TransactionPayment.PaymentMethodID == 1 {
+		vaNumber = response.PermataVaNumber
+	} else {
+		vaNumber = response.VaNumbers[0].VANumber
+	}
+
+	return &domain.Transaction{
+		CashierID: transaction.CashierID,
+		MembershipID:       transaction.MembershipID,
+		ConvertPointID :    transaction.ConvertPointID,
+		Discount: 			transaction.Discount,
+		TotalPrice: 		transaction.TotalPrice,
+		Tax: 				transaction.Tax,
+		TotalPayment: 		transaction.TotalPayment,
+		Details:   			transaction.Details,
+		TransactionPayment: domain.TransactionPayment{
+			ID: transaction.TransactionPayment.ID,
+			TransactionID: transaction.TransactionPayment.TransactionID,
+			CreatedAt: parseTransactionTime,
+			UpdatedAt: parseTransactionTime,
+			PaymentMethodID: transaction.TransactionPayment.PaymentMethodID,
+			PaymentMethod: transaction.TransactionPayment.PaymentMethod,
+			Invoice: transaction.TransactionPayment.Invoice,
+			PaymentStatus: response.TransactionStatus,
+			VANumber: vaNumber,
+		},
+	}
 }
