a118e4071f848ea3ad80c7ada3f1abb93d46c748
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 16:33:13 +0700

    feat(paymentType):update web request

--- models/web/paymentTypeRequest.go
+++ models/web/paymentTypeRequest.go
@@ -5,9 +5,15 @@ type PaymentTypeRequest struct {
 	TypeName string `gorm:"not null" json:"typeName"`
 =======
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 	TypeName string `gorm:"not null" json:"typeName"`
 =======
 	TypeName string `json:"typeName" validate:"required"`
+=======
+	TypeName string `json:"typeName" validate:"required"`
+=======
+	TypeName string `json:"typeName"`
+>>>>>>> Stashed changes
 >>>>>>> Stashed changes
 >>>>>>> Stashed changes
 }
