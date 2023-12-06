0328c4cd0aa94193013f57f4963c5b767c2d2f7d
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 16:29:29 +0700

    feat(membership): update web request

--- models/web/membershipRequest.go
+++ models/web/membershipRequest.go
@@ -8,9 +8,58 @@ type MembershipCreateRequest struct {
 
 
 type MembershipUpdateRequest struct {
+<<<<<<< Updated upstream
 	CashierID   uint   `json:"cashierId"`
 	Name        string `json:"name" validate:"required,min=1,max=255"`
 	Point       int    `json:"point" validate:"numeric"`
 	PhoneNumber string `json:"phoneNumber" validate:"required,number,min=1,max=15"`
 }
 
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+	CashierID   uint   `json:"CashierID"`
+	Name        string `json:"name" validate:"required,min=1,max=255"`
+	Point       uint   `json:"point"`
+	PhoneNumber string `json:"phoneNumber" validate:"required,min=1,max=15"`
+}
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+	CashierID	uint	`json:"CashierID"`
+	Name		string 	`json:"name" validate:"required,min=1,max=255"`
+	Point       uint    `json:"point"`
+	Telephone	string	`json:"telephone" validate:"required,min=1,max=15"`
+<<<<<<< Updated upstream
+}
+=======
+}
+=======
+	CashierID   uint   `json:"CashierID"`
+	Name        string `json:"name" validate:"required,min=1,max=255"`
+<<<<<<< Updated upstream
+	Point       uint   `json:"point"`
+	PhoneNumber string `json:"phoneNumber" validate:"required,min=1,max=15"`
+=======
+<<<<<<< Updated upstream
+	Point       int    `json:"point" validate:"numeric"`
+	PhoneNumber string `json:"phoneNumber" validate:"required,number,min=1,max=15"`
+>>>>>>> Stashed changes
+}
+
+=======
+	Point       uint   `json:"point"`
+	PhoneNumber string `json:"phoneNumber" validate:"required,min=1,max=15"`
+<<<<<<< Updated upstream
+}
+=======
+}
+
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
