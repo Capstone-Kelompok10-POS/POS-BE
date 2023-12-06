39cfbbad2744b18c74b7a688093e24e2b2a6ecac
Mohammad Arief Lazuardi <113014584+AriefLazuardi@users.noreply.github.com>
2023-12-06 16:44:18 +0700

    chore(database.go): fix conflict merge

--- drivers/database.go
+++ drivers/database.go
@@ -30,83 +30,21 @@ func NewMySQLConnection(config *configs.MySQLConfig) (*gorm.DB, error) {
 
 }
 
-<<<<<<< Updated upstream
-func Migrate() {
-	err := DB.AutoMigrate(
-=======
 func Migrate(db *gorm.DB) {
 	db.AutoMigrate(
->>>>>>> Stashed changes
 		&schema.SuperAdmin{},
 		&schema.Admin{},
-<<<<<<< Updated upstream
 		&schema.ConvertPoint{},
-=======
-<<<<<<< Updated upstream
-		&schema.ProductType{})
-=======
-<<<<<<< Updated upstream
-    &schema.ConvertPoint{},
-=======
-<<<<<<< Updated upstream
-		&schema.ProductType{})
-=======
-		&schema.ConvertPoint{},
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 		&schema.ProductType{},
 		&schema.Product{},
 		&schema.Stock{},
 		&schema.Cashier{},
-<<<<<<< Updated upstream
 		&schema.Membership{},
 		&schema.PaymentType{},
 		&schema.PaymentMethod{},
-		&schema.ProductDetail{})
-
-=======
-<<<<<<< Updated upstream
-		&schema.Membership{})
-
-=======
-		&schema.Membership{},
-		&schema.PaymentType{},
-<<<<<<< Updated upstream
-		&schema.PaymentMethod{},
-		&schema.ProductDetail{})
-=======
-<<<<<<< Updated upstream
-		&schema.PaymentMethod{})
-=======
-<<<<<<< Updated upstream
-		&schema.PaymentMethod{},
-		&schema.ProductDetail{})
-=======
-<<<<<<< Updated upstream
-		&schema.PaymentMethod{})
-=======
-		&schema.PaymentMethod{},
-<<<<<<< Updated upstream
-		&schema.ProductDetail{})
->>>>>>> Stashed changes
->>>>>>> Stashed changes
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-
->>>>>>> Stashed changes
->>>>>>> Stashed changes
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-	if err != nil {
-		log.Fatal("Failed to Migrate Database")
-	}
-	fmt.Println("Success Migrate Database")
-}
-=======
 		&schema.ProductDetail{},
 		&schema.Transaction{},
 		&schema.TransactionDetail{},
 		&schema.TransactionPayment{},
 	)
 }
->>>>>>> Stashed changes
