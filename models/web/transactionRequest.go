f28d3b78bffba0fb90f898bc27a18ae655fe23c1
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 15:58:54 +0700

    feat(transaction): update web request

--- models/web/transactionRequest.go
+++ models/web/transactionRequest.go
@@ -4,15 +4,19 @@ import "qbills/models/domain"
 
 
 type TransactionCreateRequest struct {
-	CashierID 	   uint 					   `json:"cashierId" validate:"numeric"`
-	MembershipID   uint                        `json:"membershipId" validate:"numeric"`
-	ConvertPointID uint                        `json:"convertPointId" validate:"numeric"`
-	Details        []domain.TransactionDetail  `json:"details" validate:"required"`
+	CashierID 	   uint 					     `json:"cashierId" validate:"numeric"`
+	MembershipID   uint                          `json:"membershipId" validate:"numeric"`
+	ConvertPointID uint                          `json:"convertPointId" validate:"numeric"`
+	Details        []domain.TransactionDetail    `json:"details" validate:"required"`
+	TotalPrice 	   float64 				         `json:"totalPrice" validate:"numeric"`
+	Discount 	   float64 						 `json:"discount" validate:"numeric"`
+	Tax 		   float64 					     `json:"tax" validate:"numeric"`
+	TotalPayment   float64 						 `json:"totalPayment" validate:"numeric"`
+	TransactionPayment domain.TransactionPayment `json:"transactionPayment" validate:"required"` 
 }
 
 type TransactionCreate struct {
 	CashierID 	 uint 		`json:"cashierId"`
-	Status 		 string 	`json:"status"`
 	Discount 	 float64 	`json:"discount"`
 	TotalPrice   float64    `json:"total"`
 	Tax 		 float64 	`json:"tax"`
