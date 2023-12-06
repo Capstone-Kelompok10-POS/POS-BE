77ac39b6b5ba5f5b9e13a82be587aa0808b54024
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 16:37:35 +0700

    feat(drivers): update to config

--- drivers/database.go
+++ drivers/database.go
@@ -2,53 +2,111 @@ package drivers
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"qbills/configs"
 	"qbills/models/schema"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
 
-func ConnectDB() {
+func NewMySQLConnection(config *configs.MySQLConfig) (*gorm.DB, error) {
 
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"))
+		config.Username,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.Database)
 
-	var errDB error
-	DB, errDB = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if errDB != nil {
-		panic("failed connect to database")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
 	}
 
-	Migrate()
+	Migrate(db)
 
-	fmt.Println("Connected to database")
+	return db, nil
 
 }
 
+<<<<<<< Updated upstream
 func Migrate() {
 	err := DB.AutoMigrate(
+=======
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(
+>>>>>>> Stashed changes
 		&schema.SuperAdmin{},
 		&schema.Admin{},
+<<<<<<< Updated upstream
 		&schema.ConvertPoint{},
+=======
+<<<<<<< Updated upstream
+		&schema.ProductType{})
+=======
+<<<<<<< Updated upstream
+    &schema.ConvertPoint{},
+=======
+<<<<<<< Updated upstream
+		&schema.ProductType{})
+=======
+		&schema.ConvertPoint{},
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 		&schema.ProductType{},
 		&schema.Product{},
 		&schema.Stock{},
 		&schema.Cashier{},
+<<<<<<< Updated upstream
 		&schema.Membership{},
 		&schema.PaymentType{},
 		&schema.PaymentMethod{},
 		&schema.ProductDetail{})
 
+=======
+<<<<<<< Updated upstream
+		&schema.Membership{})
+
+=======
+		&schema.Membership{},
+		&schema.PaymentType{},
+<<<<<<< Updated upstream
+		&schema.PaymentMethod{},
+		&schema.ProductDetail{})
+=======
+<<<<<<< Updated upstream
+		&schema.PaymentMethod{})
+=======
+<<<<<<< Updated upstream
+		&schema.PaymentMethod{},
+		&schema.ProductDetail{})
+=======
+<<<<<<< Updated upstream
+		&schema.PaymentMethod{})
+=======
+		&schema.PaymentMethod{},
+<<<<<<< Updated upstream
+		&schema.ProductDetail{})
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 	if err != nil {
 		log.Fatal("Failed to Migrate Database")
 	}
 	fmt.Println("Success Migrate Database")
 }
+=======
+		&schema.ProductDetail{},
+		&schema.Transaction{},
+		&schema.TransactionDetail{},
+		&schema.TransactionPayment{},
+	)
+}
+>>>>>>> Stashed changes
