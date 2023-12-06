9bd6a5105ae33689a902e1fe4d0af1ecd881f564
Arieflazuardii <m.arieflazuardi@gmail.com>
2023-12-06 16:11:25 +0700

    feat(cashier): update web cashier

--- models/web/cashierResponse.go
+++ models/web/cashierResponse.go
@@ -12,14 +12,26 @@ type CashierResponse struct {
 	AdminID  uint   `json:"adminID"`
 	Fullname string `json:"fullname"`
 	Username string `json:"username"`
+<<<<<<< Updated upstream
 =======
 	AdminID  uint   `json:"adminId"`
 	Fullname string `json:"fullname"`
 	Username string `json:"username"`
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
 }
 
 type CashierTransactionResponse struct {
 	ID       uint   `json:"id"`
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+	Fullname string `json:"fullname"`
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 	Username string `json:"username"`
 >>>>>>> Stashed changes
 }
